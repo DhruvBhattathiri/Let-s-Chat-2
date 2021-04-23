cc3d5826ef9e59dcdece7780ef50aa262586ffda
DhruvBhattathiri <73540837+DhruvBhattathiri@users.noreply.github.com>
2021-04-23 12:31:31 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBunF2ExW9PhbYN7HguvEQPOhUWhBwut9Y",
+    authDomain: "let-s-chat-92ffb.firebaseapp.com",
+    projectId: "let-s-chat-92ffb",
+    storageBucket: "let-s-chat-92ffb.appspot.com",
+    messagingSenderId: "199954932492",
+    appId: "1:199954932492:web:62435017b31d6af25ddf29",
+    measurementId: "G-NY410ZGJQ2"
+  };
+
+  firebase.initializeApp(firebaseConfig);
